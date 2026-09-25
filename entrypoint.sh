82ab04226a9aad49cc4cc329565d6ed2eddea668
WobwobRt <140562858+WobwobRt@users.noreply.github.com>
2026-09-25 13:38:04 +0200

    Allow the UNIMUS_API_KEY to be provided by a secret (unimus_api_key)

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,10 @@ set -e
 
 INTERVAL="${EXPORT_INTERVAL_SECONDS:-86400}"  # default: once a day
 
+if [ -f /run/secrets/unimus_api_key ]; then
+  UNIMUS_API_KEY=$(cat /run/secrets/unimus_api_key)
+fi
+
 cat > /exporter/unimus-backup-exporter.env <<EOF
 unimus_server_address="${UNIMUS_SERVER_ADDRESS}"
 unimus_api_key="${UNIMUS_API_KEY}"
